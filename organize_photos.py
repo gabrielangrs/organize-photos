d51ccde45be121538289856a2d51ac99e4f6d96c
Gabriel Ang <gabriel.ang@gmail.com>
2019-08-26 19:34:09 +0800

    Created function for extracting place

--- organize_photos.py
+++ organize_photos.py
@@ -5,13 +5,13 @@ os.chdir("Photos")
 # Assign variable 'originals' to list the contents of directory
 originals = os.listdir()
 
-# Find first underscore in a string
-first = filename.find("_")
-# Slice from after first underscore and assign to variable 'partial'
-partial = filname[first+1:]
-
-# Find second underscore from leftover string
-second = partial.find("_")
-destination = partial[:second]
+def extract_place(filename):
+	# Find first underscore in a string
+	first = filename.find("_")
+	# Slice from after first underscore and assign to variable 'partial'
+	partial = filename[first+1:]
+	# Find second underscore from leftover string
+	second = partial.find("_")
+	return partial[:second]
 
 print(originals)
