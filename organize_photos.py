26683a9e7e8415ff1db1c5912b3446521c514ce7
Gabriel Ang <gabriel.ang@gmail.com>
2019-08-26 19:28:34 +0800

    Added code for finding and slicing filename strings

--- organize_photos.py
+++ organize_photos.py
@@ -1,6 +1,17 @@
 import os
 
+# Change working directory to Photos
 os.chdir("Photos")
+# Assign variable 'originals' to list the contents of directory
 originals = os.listdir()
+
+# Find first underscore in a string
+first = filename.find("_")
+# Slice from after first underscore and assign to variable 'partial'
+partial = filname[first+1:]
+
+# Find second underscore from leftover string
+second = partial.find("_")
+destination = partial[:second]
 
 print(originals)
