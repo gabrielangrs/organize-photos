b65381de541d3b3d2c700263461182857d9d7ca5
Gabriel Ang <gabriel.ang@gmail.com>
2019-08-27 13:08:45 +0800

    Modified code to accept any directory name for organizing photos

--- organize_photos.py
+++ organize_photos.py
@@ -9,23 +9,24 @@ def make_place_directories(places):
 	for dir_name in places:
 		os.mkdir(dir_name)
 
-# Change working directory, list directory and assign that list to a variable
-os.chdir("Photos")
-originals = os.listdir()
-# Empty list for extracted places
-places = []
+def organize_photos(directory):
+	# Change working directory, list directory and assign that list to a variable
+	os.chdir(directory)
+	originals = os.listdir()
+	# Empty list for extracted places
+	places = []
 
-# Runs function to extract place name from original list and appends them to an empty list
-for filename in originals:
-	place = extract_place(filename)
-	# Checks if place name already exists, otherwise add it into list
-	if place not in places:
-	    places.append(place)
+	# Runs function to extract place name from original list and appends them to an empty list
+	for filename in originals:
+		place = extract_place(filename)
+		# Checks if place name already exists, otherwise add it into list
+		if place not in places:
+		    places.append(place)
 
-# Makes new directories using places list
-make_place_directories(places)
+	# Makes new directories using places list
+	make_place_directories(places)
 
-# Runs function to extract place name from original list, appends the path and moves the file into that path
-for filename in originals:
-	place = extract_place(filename)
-	os.rename(filename, os.path.join(place, filename))
+	# Runs function to extract place name from original list, appends the path and moves the file into that path
+	for filename in originals:
+		place = extract_place(filename)
+		os.rename(filename, os.path.join(place, filename))
