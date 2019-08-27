19f1f02752e6eb44c6047c49900d8aac378dcae9
Gabriel Ang <gabriel.ang@gmail.com>
2019-08-27 12:11:57 +0800

    Loop over original list to extract and append

--- organize_photos.py
+++ organize_photos.py
@@ -1,17 +1,11 @@
 import os
 
-# Change working directory to Photos
-os.chdir("Photos")
-# Assign variable 'originals' to list the contents of directory
-originals = os.listdir()
-
 def extract_place(filename):
-	# Find first underscore in a string
-	first = filename.find("_")
-	# Slice from after first underscore and assign to variable 'partial'
-	partial = filename[first+1:]
-	# Find second underscore from leftover string
-	second = partial.find("_")
-	return partial[:second]
+    return filename.split("_")[1]
 
-print(originals)
+os.chdir("Photos")
+originals = os.listdir()
+places = []
+
+for filename in originals:
+    places.append(extract_place(filename))
